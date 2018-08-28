2cf4cdf3012cd33a7f9341480a925cfa58447d3f
nightwarrior-xxx <nightwarrior-xxx@debian>
2018-08-28 20:14:29 +0530

    added changes to level8

--- level8/level8.sh
+++ level8/level8.sh
@@ -1,24 +1,28 @@
 #! /bin/bash
 n=0
 m=0
-while [ "$n" -lt "20" ]
+while [ "$n" -lt "50" ]
     do mkdir "dir $n"
         cd    "dir $n"
         touch flag.txt
+        touch password.txt
+        mkdir osdc_facebook.txt
         cd ..
         n=$(( n+1 ))
     done
-mkdir "dir 20"
-cd "dir 20" 
+mkdir "dir 50"
+cd "dir 50" 
 touch flag.txt
 echo "556a27092b57b87d26716da9f35cbb37" >> flag.txt
 cd ..
-m=$(( m+21 ))
+m=$(( m+51 ))
 
-while [ "$m" -lt "41" ]
+while [ "$m" -lt "101" ]
     do mkdir "dir $m"
         cd   "dir $m"
         touch flag.txt
+        mkdir nightwarriorxxx
+        touch nightwarrior-xxx.txt
         cd ..
         m=$((m+1))
     done
